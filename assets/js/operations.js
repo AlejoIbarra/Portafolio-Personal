331e3d079e0d6268a6d25d61f4ad66e7545ba1f9
Alejandro Ibarra <andresibarra@umariana.edu.co>
2024-04-28 23:48:58 -0500

    add vertical scrolling to title and add name change function

--- assets/js/operations.js
+++ assets/js/operations.js
@@ -21,5 +21,32 @@ const calculateAge = () => {
     }
 };
 
-// Call the function to calculate age on page load
 window.addEventListener('load', calculateAge);
+
+
+// Función para cambiar el título cuando se produce un evento de cambio de pestaña
+let intervalId;
+
+// cambiar el título y desplazarlo de derecha a izquierda
+const changeTitle = (newTitle) => {
+    clearInterval(intervalId); // Detener el intervalo anterior si existe
+    document.title = newTitle;
+    // Iniciar un intervalo para desplazar el título
+    let pos = 0;
+    intervalId = setInterval(() => {
+        pos++;
+        document.title = newTitle.substring(pos) + newTitle.substring(0, pos);
+        if (pos >= newTitle.length) {
+            pos = 0;
+        }
+    }, 200);
+};
+
+// Detectar cambios de visibilidad para cambiar el título
+document.addEventListener('visibilitychange', () => {
+    if (document.visibilityState === 'hidden') {
+        changeTitle("🙋‍♂️ Im Here att: Alejo"); // Cambiar el título cuando la pestaña está oculta
+    } else {
+        changeTitle(" Ing. Andres Alejandro Ibarra"); // Restaurar el título cuando la pestaña vuelve a estar visible
+    }
+});
